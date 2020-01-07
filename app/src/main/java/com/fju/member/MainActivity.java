c5294bca6b399513fe758dc9b4ed8ffbe1613749
miia <miia@com.tw>
2020-01-07 14:01:38 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,13 +2,20 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    boolean login = false;
+    public static final int RC_LOGIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!login){
+            Intent intent = new Intent(this,NicknameActivity.class);
+            startActivityForResult(intent,RC_LOGIN);
+        }
     }
 }
